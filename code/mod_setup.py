07b15420ef0d9610601b0784aadca33296f06e7e
Aaron C Wagner <aaron.c@thewagners.family>
2024-10-07 19:10:23 -0400

    update mod import setup

--- code/mod_setup.py
+++ code/mod_setup.py
@@ -9,6 +9,18 @@ def display_mods():
     loc=50
     for mod in mods:
         mod_path=path.mod_path(path.main+mod)
-        image=pg.image.load(mod_path+'icon.png')
+        try:
+            image=pg.image.load(mod_path+'icon.png')
+        except:
+            image=error_texture
+        try:
+            f=open(mod_path+'title.txt','r')
+            title=f.read()
+            f.close()
+        except:
+            title=mod
         screen.blit(image,(710,loc))
+        name=font.render(title,True,(255,255,255))
+        screen.blit(name,(710,loc+525))
+        loc+=575
 display_mods()
